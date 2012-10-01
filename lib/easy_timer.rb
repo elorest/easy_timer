a0308467114d69f600fcf01602efe8563a94d022
Isaac Sloan <isaac@isaacsloan.com>
2012-10-01 15:26:28 -0600

    added option to show or hide milliseconds

--- lib/easy_timer.rb
+++ lib/easy_timer.rb
@@ -5,7 +5,7 @@ module EasyTimer
     base.extend(ClassMethods)
   end
 
-	module ClassMethods
+  module ClassMethods
     def timer(options = {}, &block)
       start_time = Time.now
       yield
@@ -16,21 +16,21 @@ module EasyTimer
         return total_time
       end
     end
-	end
+  end
 
-  def verbose
+  def verbose(options = {:msec => true})
     time_hash = {}
     seconds = self.to_i
-    milliseconds = (self.to_f - seconds)
     time_hash[:week], seconds = seconds.divmod(604800)
     time_hash[:day], seconds = seconds.divmod(86400)
     time_hash[:hour], seconds = seconds.divmod(3600)
     time_hash[:minute], seconds = seconds.divmod(60)
-    time_hash[:second] = seconds + milliseconds
-    return time_hash.select{|k,v| v > 0}.inject([]){|verbose, array|
+    time_hash[:second] = seconds
+    time_hash[:second] += (self.to_f - self.to_i) if options[:msec]
+    return time_hash.select{|k,v| v > 0}.inject([]) do |verbose, array|
       k, v = array
       verbose << "#{v.is_a?(Float) ? sprintf("%.2f", v) : v} #{k.to_s + (v == 1 ? '' : 's')}"
-    }.join(" ")
+    end.join(" ")
   end
 end
 
